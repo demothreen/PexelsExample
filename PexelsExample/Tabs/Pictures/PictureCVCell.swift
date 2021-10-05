425ce3b4f8b71f215c4a97b641a01fe50d30c9ab
demothreen <demonnn@list.ru>
2021-10-05 22:26:48 +0500

    add photographer name in cell

--- PexelsExample/Tabs/Pictures/PictureCVCell.swift
+++ PexelsExample/Tabs/Pictures/PictureCVCell.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class PictureCVCell: UICollectionViewCell {
-  let imageView: UIImageView = {
+  private let imageView: UIImageView = {
     let image = UIImageView()
     image.contentMode = .scaleAspectFill
     return image
+  }()
+  private var photographerName: UILabel = {
+    let label = UILabel()
+    label.textColor = .white
+    label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+    label.backgroundColor = .black.withAlphaComponent(0.4)
+    return label
   }()
   var photo: Photo? {
     didSet {
@@ -22,6 +29,7 @@ class PictureCVCell: UICollectionViewCell {
   override init(frame: CGRect) {
     super.init(frame: .zero)
     setImageView()
+    setPhotographerName()
   }
 
   private func setImageView() {
@@ -31,9 +39,20 @@ class PictureCVCell: UICollectionViewCell {
     }
   }
 
+  private func setPhotographerName() {
+    imageView.addSubview(photographerName)
+    photographerName.snp.makeConstraints { make in
+      make.left.bottom.right.equalTo(imageView)
+      make.height.equalTo(16)
+    }
+  }
+
   private func setPhotoData() {
     if let photoData = photo {
       imageView.downloaded(from: URL(string: photoData.src.small)!)
+      photographerName.text = photoData.photographer
+      imageView.layer.cornerRadius = 10
+      imageView.layer.masksToBounds = true
     }
   }
 
